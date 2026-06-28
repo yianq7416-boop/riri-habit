2b0c493e1815122d16c9850af99066800f97eea1
yianq7416-boop <yianq7416-boop@users.noreply.github.com>
2026-06-29 00:40:53 +0800

    Fix collapsed shortcuts layout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "riri-v24";
-const ASSETS = ["./", "index.html", "styles.css?v=24", "app.js?v=24", "manifest.webmanifest?v=5", "icon.svg"];
+const CACHE = "riri-v25";
+const ASSETS = ["./", "index.html", "styles.css?v=25", "app.js?v=25", "manifest.webmanifest?v=5", "icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
