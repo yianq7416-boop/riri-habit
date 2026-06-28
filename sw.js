08d24e403a73806bfc0097ff8250b75851f8d394
yianq7416-boop <yianq7416-boop@users.noreply.github.com>
2026-06-28 22:06:59 +0800

    Refine mobile layout for daily use

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "riri-v8";
-const ASSETS = ["./", "index.html", "styles.css?v=8", "app.js?v=8", "manifest.webmanifest?v=5", "icon.svg"];
+const CACHE = "riri-v10";
+const ASSETS = ["./", "index.html", "styles.css?v=10", "app.js?v=8", "manifest.webmanifest?v=5", "icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
